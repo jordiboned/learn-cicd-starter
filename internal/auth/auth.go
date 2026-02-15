ed130d5e2c8feaf00879fec80e5ec843398f0b22
jordiboned <jordiparkour@hotmail.com>
2026-02-15 23:06:59 +0100

    Restored auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,6 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	// return splitAuth[1], nil
-	return "siempre lo mismo",nil  // Forzar error
+	 return splitAuth[1], nil
 }
